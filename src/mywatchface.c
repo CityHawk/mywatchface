83ebe71c555240a5695b58da8a97689f7321bf56
Eugene Suchkov <eugene.suchkov@gmail.com>
2016-08-15 07:24:29 -0400

    initial commit

---
+++ src/mywatchface.c
@@ -0,0 +1,107 @@
+#include "mywatchface.h"
+#include <pebble.h>
+
+static Window *s_window;
+static TextLayer *s_text_layer;
+static Layer *s_hands_layer;
+static GFont s_time_font;
+static GPath *s_minute_arrow, *s_hour_arrow;
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
+static void hands_update_proc(Layer *layer, GContext *ctx);
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_text_layer, s_buffer);
+}
+
+
+static void prv_window_load(Window *window) {
+
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+
+    s_hands_layer = layer_create(GRect(0,144,0,144));
+    layer_set_update_proc(s_hands_layer, hands_update_proc);
+    layer_add_child(window_layer, s_hands_layer);
+
+    // Redraw this as soon as possible
+    layer_mark_dirty(s_hands_layer);
+
+
+    s_text_layer = text_layer_create(GRect(0, 144, bounds.size.w, bounds.size.h));
+    text_layer_set_text_alignment(s_text_layer, GTextAlignmentLeft);
+    layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_22));
+
+    // Apply to TextLayer
+    text_layer_set_font(s_text_layer, s_time_font);
+
+}
+
+static void prv_window_unload(Window *window) {
+    text_layer_destroy(s_text_layer);
+    // Unload GFont
+    fonts_unload_custom_font(s_time_font);
+}
+
+
+static void prv_init(void) {
+    // Register with TickTimerService
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+    s_window = window_create();
+    window_set_window_handlers(s_window, (WindowHandlers) {
+            .load = prv_window_load,
+            .unload = prv_window_unload,
+            });
+    const bool animated = true;
+    window_stack_push(s_window, animated);
+
+
+}
+
+static void prv_deinit(void) {
+    window_destroy(s_window);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
+}
+
+static void hands_update_proc(Layer *layer, GContext *ctx) {
+    /* s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS); */
+    /* s_hour_arrow = gpath_create(&HOUR_HAND_POINTS); */
+
+    // Set the line color
+    graphics_context_set_stroke_color(ctx, GColorRed);
+
+    // Set the fill color
+    graphics_context_set_fill_color(ctx, GColorBlue);
+
+
+    GRect foo = GRect(5,5,20,20);
+    graphics_draw_rect(ctx, foo);
+    graphics_fill_rect(ctx, foo, 1,GCornersAll);
+    /* gpath_draw_filled(ctx, s_minute_arrow); */
+    /* gpath_draw_outline(ctx, s_minute_arrow); */
+
+}
+
+int main(void) {
+    prv_init();
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
+
+    app_event_loop();
+    prv_deinit();
+}
