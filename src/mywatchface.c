2e7841ceaca0f2dda4377a7818f78eacb9ea08c4
Eugene Suchkov <eugene.suchkov@gmail.com>
2016-08-16 07:25:58 -0400

    kinda prototype

--- src/mywatchface.c
+++ src/mywatchface.c
@@ -2,54 +2,39 @@
 #include <pebble.h>
 
 static Window *s_window;
-static TextLayer *s_text_layer;
 static Layer *s_hands_layer;
+static Layer *s_digital_layer;
 static GFont s_time_font;
 static GPath *s_minute_arrow, *s_hour_arrow;
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
 static void hands_update_proc(Layer *layer, GContext *ctx);
-
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-
-  // Write the current hours and minutes into a buffer
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_text_layer, s_buffer);
-}
-
+static void digital_update_proc(Layer *layer, GContext *ctx);
 
 static void prv_window_load(Window *window) {
 
+    window_set_background_color(window, GColorBlack);
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TIME_18));
 
     s_hands_layer = layer_create(GRect(0,0,144,144));
     layer_set_update_proc(s_hands_layer, hands_update_proc);
     layer_add_child(window_layer, s_hands_layer);
 
+
+    s_digital_layer = layer_create(GRect(0, 144, bounds.size.w, bounds.size.h));
+    layer_set_update_proc(s_digital_layer, digital_update_proc);
+    layer_add_child(window_layer, s_digital_layer);
+
     // Redraw this as soon as possible
     layer_mark_dirty(s_hands_layer);
-
-
-    s_text_layer = text_layer_create(GRect(0, 144, bounds.size.w, bounds.size.h));
-    text_layer_set_text_alignment(s_text_layer, GTextAlignmentLeft);
-    layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
-    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_22));
-
-    // Apply to TextLayer
-    text_layer_set_font(s_text_layer, s_time_font);
-
+    layer_mark_dirty(s_digital_layer);
 }
 
 static void prv_window_unload(Window *window) {
-    text_layer_destroy(s_text_layer);
+    layer_destroy(s_hands_layer);
+    layer_destroy(s_digital_layer);
     // Unload GFont
     fonts_unload_custom_font(s_time_font);
 }
@@ -75,8 +60,8 @@ static void prv_deinit(void) {
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-  layer_mark_dirty(s_hands_layer);
+    layer_mark_dirty(s_hands_layer);
+    layer_mark_dirty(s_digital_layer);
 }
 
 static void hands_update_proc(Layer *layer, GContext *ctx) {
@@ -89,14 +74,30 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
     gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
-    gpath_rotate_to(s_hour_arrow, TRIG_MAX_ANGLE * t->tm_hour / 60);
+    gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
 
 
+    graphics_context_set_fill_color(ctx, GColorRed);
+    gpath_draw_filled(ctx, s_hour_arrow);
+    gpath_draw_outline(ctx, s_hour_arrow);
+
+    graphics_context_set_fill_color(ctx, GColorWhite);
     gpath_draw_filled(ctx, s_minute_arrow);
     gpath_draw_outline(ctx, s_minute_arrow);
 
-    gpath_draw_filled(ctx, s_hour_arrow);
-    gpath_draw_outline(ctx, s_hour_arrow);
+    graphics_draw_circle(ctx, center, 2);
+}
+
+static void digital_update_proc(Layer *layer, GContext *ctx) {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+
+    char s_buffer[20];
+    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?  "%H:%M %a %m/%d" : "%I:%M %a %m/%d", t);
+
+    /* GTextAttributes *text_attrs = graphics_text_attributes_create(); */
+    graphics_draw_text(ctx, s_buffer, s_time_font, layer_get_bounds(layer), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+
 }
 
 int main(void) {
--- src/mywatchface.h
+++ src/mywatchface.h
@@ -1,19 +1,23 @@
 #include <pebble.h>
 
 static const GPathInfo MINUTE_HAND_POINTS = {
-  4, (GPoint []) {
-    { -3, 10 },
-    { 3, 10 },
-    { 3, -60 },
-      {-3, -60}
-  }
+    6, (GPoint []) {
+        { -3, 10 },
+        { 3, 10 },
+        { 3, -58 },
+        { 2, -60 },
+        {-2, -60},
+        {-3, -58}
+    }
 };
 
 static const GPathInfo HOUR_HAND_POINTS = {
-  4, (GPoint []){
-    {-3, 10},
-    {3, 10},
-    {3, -40},
-    {-3,-40}
-  }
+    6, (GPoint []){
+        {-3, 10},
+        {3, 10},
+        {3, -38},
+        {2, -40},
+        {-2,-40},
+        {-3, -38}
+    }
 };
